f8a3fcc6754a6513f7143ddacb57d54e942d1cf7
aysenurgencdogmus <aysenur812@hotmail.com>
2016-01-04 13:18:48 +0200

    frek.m dosyası güncellendi.

--- frek.m
+++ frek.m
@@ -1,5 +1,5 @@
  function ff=frek(nota,oktav)  %argümanlarý nota ve oktav olan frek fonksiyonu oluþturdum
- notalar={'Do','Dod', 'Re', 'Mib' ,'Mi' ,'Fa', 'Fad', 'Sol', 'Sold', 'La', 'Sib', 'Si','Sus'}; %notalar dizisi oluþturup notalarý ekledim. 
+ notalar={'do','dod', 're', 'mib' ,'mi' ,'fa', 'fad', 'sol', 'sold', 'la', 'sib', 'si','sus'}; %notalar dizisi oluþturup notalarý ekledim. 
  referans=16.35;  %do notasýnýn 0.oktavdaki deðerini referans aldým. 
  p=length(notalar); %p deðiþkenini notalar dizisinin uzunluðuna eþitledim. 
  if nargin<2 %argümanlarýn sayýsý 2 den küçükse 
@@ -10,11 +10,10 @@ for i=0:8 %oktav için bir for oluþturdum ve her seferinde bir arttýrdým.
        for j=1:p %notalar dizindeki elemanlar için for döngüsü oluþturdum 
             if  size(nota)==size(notalar{j}) %argüman olan nota boyutu eþit ise notalar dizisindeki j.elemana 
                if nota==notalar{j}%argüman olan nota, notalar dizisindeki j. elemana eþitse 
-                   ff=2^i*(2^((j-1)/12)*referans);%frekansý hesaplýyorum 
-                   if size(nota)==size(notalar{13})%argüman olan nota boyutu eþit ise notalar dizisindeki 13.elemana 
-                   if nota==notalar{13}%argüman olan nota notalar dizisindeki 13. elemana eþitse 
+                   if j==p%argüman olan nota notalar dizisindeki 13. elemana eþitse 
                        ff=0; %frekansý 0 a eþitle. 
-                    end 
+                   else
+                       ff=2^i*(2^((j-1)/12)*referans);%frekansý hesaplýyorum 
                    end 
                end  
            end 
@@ -22,8 +21,3 @@ for i=0:8 %oktav için bir for oluþturdum ve her seferinde bir arttýrdým.
      end 
  end 
  end 
-
-
-%Frek fonksiyonunun çaðýrýlmasý
-
- f=frek('Sus')  
