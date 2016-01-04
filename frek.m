dfde6cbb455d1929fd4b56b9cc6c15165b672f48
aysenurgencdogmus <aysenur812@hotmail.com>
2016-01-05 01:03:36 +0200

    frek.m dosyası düzenlendi.

--- frek.m
+++ frek.m
@@ -1,12 +1,19 @@
- function ff=frek(nota,oktav)  %argümanlarý nota ve oktav olan frek fonksiyonu oluþturdum
+%%Fonksiyon tanimladim
+function ff=frek(nota,oktav)  %argümanlarý nota ve oktav olan frek fonksiyonu oluþturdum
+
+%%Deger tanimlari
  notalar={'do','dod', 're', 'mib' ,'mi' ,'fa', 'fad', 'sol', 'sold', 'la', 'sib', 'si','sus'}; %notalar dizisi oluþturup notalarý ekledim. 
  referans=16.35;  %do notasýnýn 0.oktavdaki deðerini referans aldým. 
- p=length(notalar); %p deðiþkenini notalar dizisinin uzunluðuna eþitledim. 
+ p=length(notalar); %p deðiþkenini notalar dizisinin uzunluðuna eþitledim.
+ 
+ %%Oktav girilmemisse
  if nargin<2 %argümanlarýn sayýsý 2 den küçükse 
       oktav=4; %oktavý 4 e eþitle 
- end 
-for i=0:8 %oktav için bir for oluþturdum ve her seferinde bir arttýrdým. 
-    if i==oktav %i'nin deðeri benim giriþ yaptýðým oktav'ýn deðerine eþitse 
+ end
+ 
+ %%Frekans degerini hesaplamak icin
+for o=0:8 %oktav için bir for oluþturdum ve her seferinde bir arttýrdým. 
+    if o==oktav %i'nin deðeri benim giriþ yaptýðým oktav'ýn deðerine eþitse 
        for j=1:p %notalar dizindeki elemanlar için for döngüsü oluþturdum 
             if  size(nota)==size(notalar{j}) %argüman olan nota boyutu eþit ise notalar dizisindeki j.elemana 
                if nota==notalar{j}%argüman olan nota, notalar dizisindeki j. elemana eþitse 
