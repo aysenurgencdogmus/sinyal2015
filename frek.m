5ae7b84a3dec9f02bed7191934d002ebcfc9b637
aysenurgencdogmus <aysenur812@hotmail.com>
2016-01-05 13:23:26 +0200

    frek.m dosyasi güncellendi.

--- frek.m
+++ frek.m
@@ -1,17 +1,17 @@
-%%Fonksiyon tanimladim
+%% Fonksiyon tanimladim
 function ff=frek(nota,oktav)  %argümanlarý nota ve oktav olan frek fonksiyonu oluþturdum
 
-%%Deger tanimlari
+%% Deger tanimlari
  notalar={'do','dod', 're', 'mib' ,'mi' ,'fa', 'fad', 'sol', 'sold', 'la', 'sib', 'si','sus'}; %notalar dizisi oluþturup notalarý ekledim. 
  referans=16.35;  %do notasýnýn 0.oktavdaki deðerini referans aldým. 
  p=length(notalar); %p deðiþkenini notalar dizisinin uzunluðuna eþitledim.
  
- %%Oktav girilmemisse
+ %% Oktav girilmemisse
  if nargin<2 %argümanlarýn sayýsý 2 den küçükse 
       oktav=4; %oktavý 4 e eþitle 
  end
  
- %%Frekans degerini hesaplamak icin
+ %% Frekans degerini hesaplamak icin
 for okt=0:8 %oktav için bir for oluþturdum ve her seferinde bir arttýrdým. 
     if okt==oktav %i'nin deðeri benim giriþ yaptýðým oktav'ýn deðerine eþitse 
        for j=1:p %notalar dizindeki elemanlar için for döngüsü oluþturdum 
@@ -20,7 +20,7 @@ for okt=0:8 %oktav için bir for oluþturdum ve her seferinde bir arttýrdým.
                    if j==p%argüman olan nota notalar dizisindeki 13. elemana eþitse 
                        ff=0; %frekansý 0 a eþitle. 
                    else
-                       ff=2^i*(2^((j-1)/12)*referans);%frekansý hesaplýyorum 
+                       ff=2^okt*(2^((j-1)/12)*referans);%frekansý hesaplýyorum 
                    end 
                end  
            end 
